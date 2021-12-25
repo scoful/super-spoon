837b7cd80a36954f326e655969925fdaf53e183d
scoful.tang <Infox123456>
2021-12-25 18:49:28 +0800

    新增csdn水阅读量

---
+++ csdn_water_bot.py
@@ -0,0 +1,45 @@
+"""
+CSDN水阅读量脚本
+by scoful
+"""
+'''
+cron: 0 1 * * * csdn_water_bot.py
+new Env('CSDN水阅读量');
+'''
+
+import datetime
+import os
+import sys
+
+import requests
+
+# 环境变量中用于存放cookie的key值，多个号用|分隔
+KEY_OF_COOKIE = "CSDN_ARTICLE_ID"
+
+
+def logout(self):
+    print("[{0}]: {1}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self))
+    sys.stdout.flush()
+
+
+def water(url):
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+    }
+    try:
+        r = requests.get(url, headers=header,
+                         timeout=10)
+    except Exception as e:
+        logout("failed")
+    else:
+        logout("successful")
+    return None
+
+
+if __name__ == '__main__':
+    cookies = os.environ[KEY_OF_COOKIE]
+    cookieList = cookies.split("&")
+    logout("检测到{}个文章记录\n开始水".format(len(cookieList)))
+    for c in cookieList:
+        water("https://blog.csdn.net/Scoful/article/details/" + c)
+    logout("CSDN刷阅读量结束")
